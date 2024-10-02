67df270997b2f7dd362afa1d6a1679558463d5f5
TheStringKing <dandreopperman@gmail.com>
2024-10-02 11:12:00 +0100

    added GET expenses by user_id endpoint, as well as associated tests in the app.test.js file

--- app/controllers/expenses-controller.js
+++ app/controllers/expenses-controller.js
@@ -4,7 +4,6 @@ exports.getExpenses = (req, res, next) => {
   const { user_id } = req.params;
   selectExpenses(user_id)
     .then((expenses) => {
-      console.log(expenses);
       res.status(200).send({ expenses });
     })
     .catch((err) => next(err));
