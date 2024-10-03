c69bf1d055613280dbab9b88667229e497c8ceab
joshuatdr <joshuaroper513@gmail.com>
2024-10-04 00:49:53 +0100

    successful login now returns a JWT containing user_id

--- app/controllers/login-controller.js
+++ app/controllers/login-controller.js
@@ -1,10 +1,14 @@
 const { comparePass } = require("../models/login-model");
+const { generateToken } = require("../services/login-service");
 
 exports.login = (req, res, next) => {
   const { email, password } = req.body;
   comparePass(email, password)
-    .then(({ user_id }) => {
-      res.status(200).send({ user_id });
+    .then((user_id) => {
+      return generateToken(user_id, "1h");
+    })
+    .then((token) => {
+      res.status(200).send({ token });
     })
     .catch((err) => next(err));
 };
