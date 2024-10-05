081e2131e3d5a7f59258bf01dd813d822a590f2d
joshuatdr <joshuaroper513@gmail.com>
2024-10-05 16:12:18 +0100

    add further test data for user goals

--- db/test-data/goals.js
+++ db/test-data/goals.js
@@ -1,20 +1,10 @@
 module.exports = [
   {
     user_id: 1,
-    name: "Japan Trip",
-    cost: 1700,
-    img_url:
-      "https://lp-cms-production.imgix.net/2023-02/GettyImages-1071391480.jpeg",
-    created_at: 1727793481265,
-    description:
-      "A 7 day trip to explore Tokyo's futuristic skyline, then escape to Kyoto's peaceful temples and gardens.",
-  },
-  {
-    user_id: 1,
     name: "New Fridge",
     cost: 599,
     img_url:
-      "https://media.currys.biz/i/currysprod/Should-I-Buy-A-Smart-Fridge-Header",
+      "https://images.pexels.com/photos/4058699/pexels-photo-4058699.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     created_at: 1727729688038,
     description: "One of those with the touchscreen that nobody ever uses.",
   },
@@ -23,9 +13,79 @@ module.exports = [
     name: "Perfume for Mum's Bday",
     cost: 145,
     img_url:
-      "https://media.theperfumeshop.com/medias/sys_master/prd-images/h29/hcc/9226375692318/prd-front-1002906_420x420/chanel-coco-mademoiselle-unknown-for-her-420x420.jpg",
+      "https://images.pexels.com/photos/9957571/pexels-photo-9957571.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     created_at: 1727698932592,
     description: "Chanel Coco Mademoiselle.",
+  },
+  {
+    user_id: 1,
+    name: "Buy a Laptop for Work",
+    cost: 1200,
+    img_url:
+      "https://images.pexels.com/photos/12899188/pexels-photo-12899188.jpeg",
+    created_at: 1727862481265,
+    description:
+      "A powerful new laptop for remote work and productivity, perfect for long-term projects.",
+  },
+  {
+    user_id: 1,
+    name: "Day Trip to the Countryside",
+    cost: 150,
+    img_url:
+      "https://images.pexels.com/photos/842711/pexels-photo-842711.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    created_at: 1727856481265,
+    description:
+      "A relaxing day trip to enjoy nature, fresh air, and scenic views in the nearby countryside.",
+  },
+  {
+    user_id: 1,
+    name: "Iceland's Northern Lights",
+    cost: 2700,
+    img_url:
+      "https://images.pexels.com/photos/91216/pexels-photo-91216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    created_at: 1727853481265,
+    description:
+      "A 5-day trip to Reykjavik to see the Northern Lights and relax in the Blue Lagoon's geothermal waters.",
+  },
+  {
+    user_id: 1,
+    name: "Weekend Getaway with Friends",
+    cost: 600,
+    img_url:
+      "https://images.pexels.com/photos/8070818/pexels-photo-8070818.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    created_at: 1727817481265,
+    description:
+      "A short weekend trip to reconnect with friends, relax, and have fun at a nearby resort or cabin.",
+  },
+  {
+    user_id: 1,
+    name: "New Smartphone",
+    cost: 1000,
+    img_url:
+      "https://images.pexels.com/photos/19936699/pexels-photo-19936699/free-photo-of-man-hand-holding-colorful-smartphones.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    created_at: 1727843481265,
+    description:
+      "A new smartphone with the latest technology, ideal for work, photography, and staying connected.",
+  },
+  {
+    user_id: 1,
+    name: "Family Christmas Gifts",
+    cost: 500,
+    img_url:
+      "https://images.pexels.com/photos/1666065/pexels-photo-1666065.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    created_at: 1727888481265,
+    description:
+      "Buying thoughtful Christmas presents for family members, including personalized gifts and holiday surprises.",
+  },
+  {
+    user_id: 1,
+    name: "Upgrade Living Room Furniture",
+    cost: 1200,
+    img_url:
+      "https://images.pexels.com/photos/2440471/pexels-photo-2440471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    created_at: 1727864481265,
+    description:
+      "New furniture for the living room, including a modern couch and coffee table for a cozy, stylish home.",
   },
   {
     user_id: 3,
